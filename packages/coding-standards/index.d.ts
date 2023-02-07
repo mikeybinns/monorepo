f9b7ad11a5d8c28bd5796e8996658c8c42d6decc
Mikey Binns <mikey.binns@atomicsmash.co.uk>
2023-02-07 11:18:29 +0000

    feat: add types for coding standards

---
+++ packages/coding-standards/index.d.ts
@@ -0,0 +1,4 @@
+export const eslintConfig: import("eslint").Linter.Config;
+export const commitlintConfig: import("@commitlint/types").UserConfig;
+export const cspellConfig: import("@cspell/cspell-types").CSpellUserSettings;
+export const prettierConfig: import("prettier").Config;
