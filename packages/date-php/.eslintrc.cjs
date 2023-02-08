9202155d024e302b683bd368cfde9574474e1bdd
Mikey Binns <mikey.binns@atomicsmash.co.uk>
2023-02-08 14:54:14 +0000

    fix: fix relative path to config

--- packages/date-php/.eslintrc.cjs
+++ packages/date-php/.eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-	extends: ["./packages/coding-standards/.eslintrc.cjs"],
+	extends: ["../coding-standards/.eslintrc.cjs"],
 	parserOptions: {
 		tsconfigRootDir: __dirname,
 		project: ["./tsconfig.json"],
