4e76e7a07dbadcfc4390b2cb9ca251c603d5752f
Mikey Binns <mikey.binns@atomicsmash.co.uk>
2023-03-09 11:54:25 +0000

    feat: add dev config

--- packages/coding-standards/browserslist/index.js
+++ packages/coding-standards/browserslist/index.js
@@ -1,1 +1,12 @@
-module.exports = ["defaults and >1% and not samsung > 0 and not op_mini all"];
+module.exports = {
+	development: [
+		"last 1 Chrome version",
+		"last 1 Firefox version",
+		"last 1 Safari version",
+		"last 1 Edge version",
+		"last 1 Safari version",
+		"last 1 ChromeAndroid version",
+		"last 1 ios_saf version",
+	],
+	production: ["defaults and >1% and not samsung > 0 and not op_mini all"],
+};
